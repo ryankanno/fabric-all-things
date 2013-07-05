75220c69e6012e2ff5ca6f5b3d38b3c37d140696
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-07-05 05:36:01 -0400

    Added helpers to __init__.py Fixed up service.py

--- fabric_all_things/__init__.py
+++ fabric_all_things/__init__.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from apt import *
+from aws import *
+from fs import *
+from git import *
 from service import *
+from virtualenv import *
+from web import *
+
+# vim: filetype=python
--- fabric_all_things/service.py
+++ fabric_all_things/service.py
@@ -3,9 +3,9 @@
 
 from fabric.api import settings
 from fabric.api import sudo
-from fabric.api import task
 
-def _service(name, action, warn_only=False):
+
+def service(name, action, warn_only=False):
     """ Generic service function """
     with settings(warn_only=warn_only):
         return sudo("/etc/init.d/{service} {action}".format(
