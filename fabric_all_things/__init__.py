9f302efb5e1213a4126509621c79dc5b0852c6a4
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-09-24 22:32:06 -0400

    Added namespaced aws tasks

--- fabric_all_things/__init__.py
+++ fabric_all_things/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from apt import *
-from aws import *
-from fs import *
-from git import *
-from service import *
-from virtualenv import *
-from web import *
+import aws
 
 # vim: filetype=python
--- fabric_all_things/aws.py
+++ fabric_all_things/aws.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from fabric.api import task
+
+@task
+def import_key_pair(key_name, path_to_public_key):
+    """
+    Imports AWS key pair from local machine into region
+    """
+    conn = _get_region_connection(
+        AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION)
+
+    with open(path_to_public_key, "rb") as public_key_file:
+        encoded_public_key = public_key_file.read()
+        conn.import_key_pair(key_name, encoded_public_key)
+
+
+def _get_region_connection(aws_access_key_id, aws_secret_access_key, region_name):
+    return connect_to_region(
+        region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+
 # vim: filetype=python
