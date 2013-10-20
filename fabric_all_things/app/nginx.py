1da0894fcf804d86b2d655a49be7e236c4db4b61
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-10-20 09:09:01 -0400

    Added display of sites, enable/disable of sites

--- fabric_all_things/app/nginx.py
+++ fabric_all_things/app/nginx.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from fabric.api import sudo
+from fabric.api import task
 import fabric_all_things.service as service
+import fabric_all_things.system as system
 
 
 class NginxCommand(service.ServiceCommand):
@@ -13,5 +16,31 @@ start = NginxCommand('start', 'Starts nginx', roles=['www-nginx'])
 stop = NginxCommand('stop', 'Stops nginx', roles=['www-nginx'])
 reload = NginxCommand('reload', 'Reload nginx', roles=['www-nginx'])
 restart = NginxCommand('restart', 'Restart nginx', roles=['www-nginx'])
+
+
+@task
+def sites():
+    """Display nginx enabled sites"""
+    command = 'ls -la /etc/nginx/sites-enabled'
+    system.system_command(command, sudo)
+
+
+@task
+def enable(site):
+    """ Creates symlink from sites-available to sites-enabled """
+    available = "/etc/nginx/sites-available/{site}".format(site=site)
+    enabled = "/etc/nginx/sites-enabled/{site}".format(site=site)
+    command = "ln -s {available} {enabled}".format(
+        available=available, enabled=enabled)
+    system.system_command(command, sudo)
+
+
+@task
+def disable(site):
+    """ Removes symlink from sites-enabled """
+    enabled = "/etc/nginx/sites-enabled/{site}".format(site=site)
+    command = "rm {enabled}".format(enabled=enabled)
+    system.system_command(command, sudo)
+
 
 # vim: filetype=python
